28c8846f0766de9a1c6b0e3d0efa6bc95e86d17f
Sean Olson <srolson@callutheran.edu>
2019-10-29 14:31:04 -0500

    added reducers/sagas/routes

--- server/server.js
+++ server/server.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const songRouter = require('./routes/song.router.js');
 const PORT = process.env.PORT || 5000;
+
+/** ------------ IMPORT ROUTES ------------ */
+const homeRouter = require('./routes/home.router.js');
+const workRouter = require('./routers/work.router.js');
+const eventsRouter = require('./routes/events.router.js');
+const aboutRouter = require('./routes/about.router.js');
+const contactRouter = require('./routes/contact.router.js');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-app.use('/songs', songRouter);
+app.use('/home', homeRouter);
+app.use('/work', workRouter);
+app.use('/events', eventsRouter);
+app.use('/about', aboutRouter);
+app.use('/contact', contactRouter);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
